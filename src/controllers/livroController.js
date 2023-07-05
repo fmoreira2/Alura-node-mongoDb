e902485e7873328d10d28f4276079a8e08e6bf30
Fernando Moreira <fernando.m.silva@outlook.com>
2023-07-04 21:31:00 -0300

    projeto_api_node_mongo_20230704

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,16 +1,26 @@
-import livros from "../models/Livro.js"; 
+import livros from "../models/Livro.js";
 
-class LivroController{
-    static listarLivros = async (req, res) =>{
-       
-        try {
-          const livrosResultado = await livros.find();
-          res.status(200).json(livrosResultado);
-        } catch (err) {
-          res.status(500).json(err);
-        }
-        
+class LivroController {
+  static listarLivros = async (req, res) => {
+    try {
+      const livrosResultado = await livros.find();
+      res.status(200).json(livrosResultado);
+    } catch (err) {
+      res.status(500).json(err);
     }
+  };
+
+  static cadastrarLivro = async (req, res) => {
+    try {
+      let livro = new livros(req.body);
+      await livro.save();
+      res.status(201).send(livro.toJSON());
+    } catch (error) {
+      res
+        .status(501)
+        .send({ message: `${error.message} - erro ao cadastrar livro` });
+    }
+  };
 }
 
 export default LivroController;
